cc4d4fa952b31b7a87fdd9f5edafd00059c30b14
edberisha <ed@peak.watch>
2024-09-16 19:06:12 -0400

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+// _app.tsx
+import { ChakraProvider } from '@chakra-ui/react';
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
+
+export default MyApp;
