5339bb31f85338616ed0ecb87676f951859e2642
edberisha <ed@peak.watch>
2024-09-17 21:12:03 -0400

    Updates

---
+++ _next/static/NUKYNPkjBQPI3zcA8pmUE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/2e3a845b-bab3d6ce5abfa23b.js","static/chunks/49-d0dc3fe5c75ed716.js","static/chunks/pages/index-a13b6680fe2bace6.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
