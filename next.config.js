9ef845a06db80166c3546e97eb5017f015c59c0a
edberisha <ed@peak.watch>
2024-09-17 20:03:33 -0400

    final hopefully

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
   reactStrictMode: true,
   output: 'export',  // This replaces 'next export'
   basePath: '/datingapp', // Set this if your site is hosted at a subpath
+  images: {
+    unoptimized: true, // Disable image optimization
+  },
 };
+
 
 module.exports = nextConfig;
