330e642363f0171ecaf1d282ac56e527dd3174d8
edberisha <ed@peak.watch>
2024-10-04 12:44:33 +0200

    removed unused variables

--- src/pages/messages.tsx
+++ src/pages/messages.tsx
@@ -1,7 +1,4 @@
-import Head from 'next/head';
-import { Box, Flex } from '@chakra-ui/react';
 import ChatAlerts from '../components/ChatAlerts';
-import Navbar from '../components/NavBar'; 
 
 function Messages() {
     return (
